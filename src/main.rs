9865a1f9dad4fdd00da0b1014f4b0a5f450dc4d9
Alan GUERET <alan.gueret@epita.fr>
2022-06-23 00:33:12 +0200

    [Parser] Start - start playing with lalrpop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+#[macro_use] extern crate lalrpop_util;
+
+lalrpop_mod!(pub calc); // synthesized by LALRPOP
+
 fn main() {
-    println!("Hello, world!");
+    print!("Hello");
+}
+
+#[test]
+fn calc() {
+    assert!(calc::TermParser::new().parse("22").is_ok());
+    // assert!(calc::TermParser::new().parse("22)").is_ok());
+    assert!(calc::TermParser::new().parse("(22)").is_ok());
+    assert!(calc::TermParser::new().parse("((((22))))").is_ok());
+    assert!(calc::TermParser::new().parse("((22)").is_err());
 }
