0c9a6cf73749f96c79718ec847a04f3e1ce8c2f9
Alan GUERET <alan.gueret@epita.fr>
2022-06-29 01:08:58 +0200

    [Parser] Parse - assign to variable

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,20 @@
 lalrpop_mod!(pub calc); // synthesized by LALRPOP
 
 fn main() {
-    print!("Hello");
+    let expr = calc::ExprParser::new().parse("A = 3 * 2 + 2").unwrap();
+
+    println!("{}", expr);
 }
 
 #[test]
 fn calc() {
-    assert!(calc::TermParser::new().parse("22").is_ok());
-    // assert!(calc::TermParser::new().parse("22)").is_ok());
-    assert!(calc::TermParser::new().parse("(22)").is_ok());
-    assert!(calc::TermParser::new().parse("((((22))))").is_ok());
-    assert!(calc::TermParser::new().parse("((22)").is_err());
+    assert!(calc::ExprParser::new().parse("A = 12").is_ok());
+    assert!(calc::ExprParser::new().parse("22").is_ok());
+    assert!(calc::ExprParser::new().parse("(22)").is_ok());
+    assert!(calc::ExprParser::new().parse("(2+2)").is_ok());
+    assert!(calc::ExprParser::new().parse("(2+2) * (2/2)").is_ok());
+    assert!(calc::ExprParser::new().parse("((((22))))").is_ok());
+    assert!(calc::ExprParser::new().parse("((22)").is_err());
+    assert!(calc::ExprParser::new().parse("((2+2)").is_err());
+    // assert!(calc::ExprParser::new().parse("A = 2").is_ok());
 }
